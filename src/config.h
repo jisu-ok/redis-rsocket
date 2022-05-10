86eb8e0548a46889c57fb0bd76d8f9f24cc23b9c
jisu <jisu@shader-moscow.anlab>
2022-05-10 17:41:54 +0900

    disable epoll and accept4

--- src/config.h
+++ src/config.h
@@ -88,12 +88,12 @@
 
 /* Test for polling API */
 #ifdef __linux__
-#define HAVE_EPOLL 1
+// #define HAVE_EPOLL 1
 #endif
 
 /* Test for accept4() */
 #ifdef __linux__
-#define HAVE_ACCEPT4 1
+// #define HAVE_ACCEPT4 1
 #endif
 
 #if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
